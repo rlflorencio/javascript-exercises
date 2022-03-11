ef723334e76bd0f7d386c113d0b38ffac007d7d1
rlflorencio <rafaellflorencio@outlook.com>
2022-03-10 21:41:38 -0300

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+	const len = input.length;
+	let output = "";
+	let ltr = "";
+	for (i = 0; i <= len; i++){
+		ltr = input.slice((len - (i + 1)), (len - i));
+		output += ltr;
+	}
+	return output
 
 };
 
