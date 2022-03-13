bd051879f890b0feb0d734d36431a42f1ef15060
rlflorencio <rafaellflorencio@outlook.com>
2022-03-13 11:28:42 -0300

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	let intYear = parseInt(year);
+	if ( intYear % 4 === 0) {
+
+		if (intYear % 100 !== 0) {
+			return true
+		} else if (intYear % 100 === 0 && intYear % 400 === 0) {
+			return true
+		} else {
+			return false
+		}
+
+	} else {
+		return false;
+	}
 
 };
 
