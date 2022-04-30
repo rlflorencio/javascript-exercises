09a8bf524a1d68f150730a057833910553beb1f6
rlflorencio <rafaellflorencio@outlook.com>
2022-04-30 11:44:33 -0300

    Completed Exercise 10_Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	let first = 0;
+	let last = 1;
+	let fibo = 0;
 
+	if (n < 0){
+		return "OOPS";
+	};
+	if (n === 1){
+		return fibo = 1;
+	};
+
+	if (!(typeof n === "number")) {
+		n = parseInt(n);
+		if (n === 1) {
+			return fibo = 1;
+		}
+		for (let i = 1; i < n; i++) {
+			fibo = first + last;
+			first = last;
+			last = fibo;
+		};
+		return fibo;
+	};
+	
+	for (let i = 1; i < n; i++) {
+		fibo = first + last;
+		first = last;
+		last = fibo;
+	};
+	return fibo;
 };
 
 // Do not edit below this line
