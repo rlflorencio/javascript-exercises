91d20d6585fa3634589c1dc948ee970cd991ba51
rlflorencio <rafaellflorencio@outlook.com>
2022-04-29 21:42:38 -0300

    Completed Exercise 08_Calculator and Exercise 09_Palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(userInput) {
+  const total = userInput.reduce((total, element) => {
+    return total + element;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let cleanStr = str.toLowerCase().replace(/[\W_]/g, '');
+	let reversed = cleanStr.split("").reverse().join("");
+	return cleanStr === reversed;
 };
 
 // Do not edit below this line
