89d8977345bbbe5089fd1353b05fd463ad17604f
rlflorencio <rafaellflorencio@outlook.com>
2022-04-30 13:49:28 -0300

    Completed Exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	const calcAge = function(obj) {
+		if (obj.yearOfDeath === undefined) {
+			obj.yearOfDeath = new Date().getFullYear();
+			obj.age = obj.yearOfDeath - obj.yearOfBirth;
+		};
+		obj.age = obj.yearOfDeath - obj.yearOfBirth;
+	};
+	
+	people.forEach(calcAge);
 
+	const oldest = people.sort(function(a,b) {
+		const first = a.age;
+		const last = b.age;
+
+		if (first>last) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
+
+	return oldest[0];
+	
 };
 
 // Do not edit below this line
