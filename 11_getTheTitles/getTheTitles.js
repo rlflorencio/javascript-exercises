c7a953291fb6d67983bd9e9aae565b0b36f5b954
rlflorencio <rafaellflorencio@outlook.com>
2022-04-30 12:11:03 -0300

    Completed Exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+	let titlesArray = array.map(book => book.title)
+	return titlesArray
 };
 
 // Do not edit below this line
